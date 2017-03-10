9de4c94e9e5e8ea6de287f2506da3f8cc79759e2
balamurugan1492 <balamurugan@pointelsolutions.com>
2017-03-10 20:55:01 +0530

    Branch Merged

--- WpfApplication1/WpfApplication1/NewFolder1/LoclClass2.cs
+++ WpfApplication1/WpfApplication1/NewFolder1/LoclClass2.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfApplication1.NewFolder1
+﻿namespace WpfApplication1.NewFolder1
 {
-    class LoclClass2
+    public class LoclClass2
     {
     }
 }
